c3b4d339da54f7ac0edae53b72baeea28bc1effe
Knudn <kor@kroland.no>
2023-02-05 21:29:44 +0000

    Updated setup script

--- tools/setup_slide.sh
+++ tools/setup_slide.sh
@@ -1,58 +1,74 @@
 #!/bin/bash
 
+# Add repository for yq tool
+add-apt-repository ppa:rmescandon/yq -y
 
+# Update packages and install required software
 apt update
-apt install postgresql postgresql-contrib -y
-apt install python3-virtualenv -y
+apt install postgresql postgresql-contrib jq python3-virtualenv -y
 
+# Create a Python virtual environment
 python3 -m venv venv
 
+# Activate virtual environment
 source venv/bin/activate
 
+# Get the current working directory
 homedir=$(pwd)
 
+# Install requirements for the slide_app
 pip install -r ../slide_app/requirements.txt
 
-#Find running instences
+# Kill any existing processes using port 5000
 netstat -tulpn | grep 5000 | awk '{print $7}' | awk -F "/" '{print $1}' | xargs kill
 
-#Remove old data
-rm -rf /tmp/slide1
-rm -rf /tmp/slide2
-rm -rf /tmp/slide3
+# Convert instances.yaml to instances.json
+cat instances.yaml | yq -j > instances.json
 
-#Make tmp data
-cp -r ../slide_app /tmp/slide1
-cp -r ../slide_app /tmp/slide2
-cp -r ../slide_app /tmp/slide3
+# Get screens data from instances.json
+screens=$(jq '.screens[]' instances.json)
 
-#Setting up the database
-sudo -u postgres ./db.sh slide1
-sudo -u postgres ./db.sh slide2
-sudo -u postgres ./db.sh slide3
+# Loop through each screen instance
+for inst in $(echo "${screens}" | jq -c '.'); do
 
-#Adding addresses
-ip address add 192.168.10.204/255.255.255.0 dev ens33
-ip address add 192.168.10.205/255.255.255.0 dev ens33
-ip address add 192.168.10.206/255.255.255.0 dev ens33
+  # Get name, IP, and local_png_dir of the screen
+  name=$(echo "${inst}" | jq -r '.name')
+  ip=$(echo "${inst}" | jq -r '.ip')
+  local_png_dir=$(echo "${inst}" | jq -r '.local_png_dir')
 
-#Amount of instences
-cd /tmp/slide1; nohup flask run -h 192.168.10.204 -p 5000 > /tmp/slide1/slide1.log 2>&1 &
-cd /tmp/slide2; nohup flask run -h 192.168.10.205 -p 5000 > /tmp/slide2/slide2.log 2>&1 &
-cd /tmp/slide3; nohup flask run -h 192.168.10.206 -p 5000 > /tmp/slide3/slide3.log 2>&1 &
+  # Remove existing /tmp/$name directory
+  rm -rf "/tmp/$name"
 
-#Create folder
-mkdir -p /tmp/slide1/slideshow/static/images/slideshow_images/
-mkdir -p /tmp/slide2/slideshow/static/images/slideshow_images/
-mkdir -p /tmp/slide3/slideshow/static/images/slideshow_images/
+  # Copy slide_app to /tmp/$name
+  cp -r ../slide_app "/tmp/$name"
 
-#Replaces the database config
-sed -i -e 's/slideshow:password@database:5432\/slideshow/slide1:slide1@127.0.0.1:5432\/slide1/g' /tmp/slide1/config.py
-sed -i -e 's/slideshow:password@database:5432\/slideshow/slide2:slide2@127.0.0.1:5432\/slide2/g' /tmp/slide2/config.py
-sed -i -e 's/slideshow:password@database:5432\/slideshow/slide3:slide3@127.0.0.1:5432\/slide3/g' /tmp/slide3/config.py
+  # Run db.sh script with name as argument
+  sudo -u postgres ./db.sh $name
 
-ps -aux | grep pdf_converte.py | awk '{print $2}'| xargs kill
-cd $homedir
-python pdf_converte.py -i 192.168.10.204 -d /share/Skjerm1 2>&1 &
-python pdf_converte.py -i 192.168.10.205 -d /share/Skjerm2 2>&1 &
-python pdf_converte.py -i 192.168.10.206 -d /share/Skjerm3 2>&1 &
+  # Add IP address to ens33 interface
+  ip address add $ip/255.255.255.0 dev ens33
+
+  # Change to /tmp/$name directory and run Flask app in background
+  cd /tmp/$name
+  nohup flask run -h $ip -p 5000 > /tmp/$name/$name.log 2>&1 &
+
+  # Create slideshow images directory
+  mkdir -p /tmp/$name/slideshow/static/images/slideshow_images/
+
+  # Replace database connection string in config.py
+  sed -i -e "s/slideshow:password@database:5432\/slideshow/$name:$name@127.0.0.1:5432\/$name/g" /tmp/$name/config.py
+
+  # Kill any existing pdf_converte.py processes
+  ps -aux | grep pdf_converte.py | awk '{print $2}'| xargs kill
+  
+  # Change back to the original working directory
+  cd $homedir
+
+  # Run pdf_converte.py in background
+  printf "\nStarting pdf_converte.py for screen '%s'\n" "$name"
+  python pdf_converte.py -i $ip -d $local_png_dir 2>&1 &
+
+done
+
+# End of script
+printf "\nScript execution completed\n"
