154b37168081e3e2f2d8249274cfa846a8de7651
Knut Olav <kor@kroland.no>
2023-02-13 01:34:11 +0000

    Update

--- tools/screen_assets/init_screens.sh
+++ tools/screen_assets/init_screens.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#sudo update
-sudo apt-get install tigervnc-scraping-server
+apt update
+apt-get install tigervnc-scraping-server xfce4 xfce4-goodies tightvncserver -y
 
-mkdir -p ~/.vnc
+mkdir -p /home/rock/.vnc
+chown rock:rock /home/rock/.vnc
+
 cd screen_assets
 
 echo "Copy Chrome script"
@@ -13,18 +15,19 @@ echo "Copy xstartup"
 cp xstartup /home/rock/.vnc/xstartup
 echo "Copy slideshow.desktop"
 cp slideshow.desktop /home/rock/.config/autostart/slideshow.desktop
-pwd
+
 chown rock:rock /home/rock/.vnc/passwd
 chown rock:rock /home/rock/.vnc/xstartup
 chown rock:rock /home/rock/chrome.sh
 chown rock:rock /home/rock/.config/autostart/slideshow.desktop
 
-echo "asd" > /root/test
 
 if  grep -q "x0vncserver" "/etc/rc.local" ; then
         echo "x0 entry exists" 
 else
         line_number=$(grep -n "exit 0" "/etc/rc.local" | tail -n 1 | awk -F: '{print $1}')
-        sudo sed -i "${line_number}i\\su rock -c 'x0vncserver :0 -localhost no'" "/etc/rc.local"
+        sudo sed -i "${line_number}i\\sleep 5; su rock -c 'x0vncserver :0 -localhost no -PasswordFile /home/rock/.vnc/passwd'" "/etc/rc.local"
 fi
 
+sleep 5
+reboot
