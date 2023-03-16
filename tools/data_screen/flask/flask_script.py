5273842458e2e983b392f21402f7c0ccd535f3af
Knut Olav <kor@kroland.no>
2023-03-16 11:30:43 +0000

    update

--- tools/data_screen/flask/flask_script.py
+++ tools/data_screen/flask/flask_script.py
@@ -10,25 +10,27 @@ cur = con.cursor()
 
 def get_event_data():
 
-    eventfile, eventex = get_event()
-    con_title, con_per = get_driver_data(eventfile)
-    startlist, time_data = get_start_list(eventex)
-    print(time_data)
-    start_list_dict = get_start_list_dict(startlist, con_per, time_data)
+    eventfile, eventex, heat = get_event()
+    print(eventfile, eventex)
+    con_title, con_per = get_driver_data(eventfile, heat)
 
+    startlist, time_data = get_start_list(eventex, heat)
+
+    start_list_dict = get_start_list_dict(startlist, con_per, time_data)
+    event_heat = [eventfile, heat]
     #startlist_json = json.dumps(start_list_dict, indent = 4) 
-    with open("startlist/" + eventfile+".json", "w") as outfile:
+    with open("startlist/" + eventfile+"_"+ heat +"_"+".json", "w") as outfile:
 
         json.dump(start_list_dict, outfile)
 
-    with open("startlist/" + eventex+"_res_.json", "w") as outfile:
+    with open("startlist/" + eventex + "_" + heat + "_res_.json", "w") as outfile:
         json.dump(time_data, outfile)
     
-    with open("startlist/" + eventex+ "_title_.json", "w") as outfile:
+    with open("startlist/" + eventex + "_" + heat + "_title_.json", "w") as outfile:
         json.dump(con_title, outfile)
 
     with open("startlist/current.json", "w") as outfile:
-        outfile.write(eventfile)
+        json.dump(event_heat, outfile)
 
     return start_list_dict, con_title, eventex
 
@@ -41,52 +43,55 @@ def startlist():
     con_title = "asd"
     active_drivers = []
     start_list = {}
+    current = []
 
-    with open('startlist/current.json', "r") as current_event:
-        current = current_event.read()
+    with open('startlist/current.json', "r") as json_file:
+        current = json.load(json_file)
 
-    with open('startlist/' + current + '.json') as json_file:
+    with open('startlist/' + current[0] + "_" + current[1] + '_.json',"r") as json_file:
         data = json.load(json_file)
 
-    with open("startlist/" + eventex+ "_title_.json") as json_file:
+    with open("startlist/" + eventex + "_" + current[1] + "_title_.json", "r") as json_file:
         title = json_file.readline()
+        print(title)
 
-    with open("startlist/" + eventex+"_res_.json") as json_file:
-        race_res = json.load(json_file)
-    
     for row in cur.execute("SELECT c_num FROM active_drivers"):
         for b in row:
             active_drivers.append(int(b))
 
-    return render_template('template_current_startlist.html', data=data, con_title=con_title, active_drivers=active_drivers, )
+
+    return render_template('template_current_startlist_2v2.html', data=data, con_title=title, active_drivers=active_drivers, )
 
 @app.route("/current_scoreboard")
 def current_scoreboard():
-    con_title = "asd"
 
     tmp_driver = []
-    with open('startlist/current.json', "r") as current_event:
-        current = current_event.read()
 
-    with open('startlist/' + current + '.json') as json_file:
+    with open('startlist/current.json', "r") as json_file:
+        current = json.load(json_file)
+    
+    with open('startlist/' + current[0] + "_" + current[1] + '_.json', "r") as json_file:
         data = json.load(json_file)
 
-    with open('startlist/current.json', "r") as current_event:
-        current = current_event.read()
+    with open("startlist/" + eventex + "_" + current[1] + "_title_.json", "r") as json_file:
+        title = json_file.readline()
     
     for a in data:
         for b in data[a]:
             #b[5] = str(seconds) + "." + str(milliseconds)
             b[5] = round(int(b[5]) / 1000, 3)
+            b[6] = str(b[6])
             tmp_driver.append(b)
-
+    
     sorted_lst = sorted(tmp_driver, key=lambda x: float('inf') if x[5] == 0.0 else x[5])
 
     fix_sorted_lst = [x for x in sorted_lst if x[5] != '0.0'] + [x for x in sorted_lst if x[5] == '0.0']
     #reversed_lst = sorted_lst[::-1]
 
 
-    return render_template('template_scoreboard.html', con_title=con_title, data=fix_sorted_lst)
+    for a in fix_sorted_lst:
+        print(type(a[6]))
+    return render_template('template_scoreboard.html', con_title=title[1:-1], data=fix_sorted_lst)
 
 
 @app.route("/new_event")
--- tools/data_screen/flask/get_active_event.py
+++ tools/data_screen/flask/get_active_event.py
@@ -1,78 +1,70 @@
 import sqlite3
+from pathlib import Path
 
-event_files = "/mnt/test/"
+event_files = Path("/mnt/test/")
 
 def get_event():
-    # Create a SQL connection to our SQLite database
-    con = sqlite3.connect(event_files+"Online.scdb")
-    cur = con.cursor()
-    # The result of a "cursor.execute" can be iterated over by row
-    for row in cur.execute('SELECT C_VALUE FROM TPARAMETERS WHERE C_PARAM="EVENT";'):
-        con.commit()
+    db_path = event_files / "Online.scdb"
+    event_values = []
 
-        if int(row[0]) < 9:
-            num = "00"+row[0]
-            return "Event"+num+".scdb", "Event"+num+"Ex.scdb"
-        elif int(row[0]) < 99:
-            num = "0"+row[0]
-            return "Event"+num+".scdb", "Event"+num+"Ex.scdb"
-        else:
-            num = row[0]
-            return "Event"+num+".scdb", "Event"+num+"Ex.scdb"
+    with sqlite3.connect(str(db_path)) as con:
+        cur = con.cursor()
+        cur.execute('SELECT C_VALUE FROM TPARAMETERS WHERE C_PARAM IN ("EVENT", "HEAT");')
+        event_values = [row[0] for row in cur.fetchall()]
+        cur.close()
 
-    # Be sure to close the connection
-    con.close()
+    num = f"{int(event_values[0]):03}"
+    return f"Event{num}.scdb", f"Event{num}Ex.scdb", event_values[1]
 
-def get_driver_data(eventfile):
-    com_list = []
+def get_driver_data(eventfile, heat):
+    db_path = event_files / eventfile
+    competitors = []
     title = "None"
-    con = sqlite3.connect(event_files+eventfile)
-    cur = con.cursor()
-    for row in cur.execute('SELECT C_NUM, C_FIRST_NAME, C_LAST_NAME, C_CLUB, C_TEAM FROM TCOMPETITORS;'):
-        com_list.append(row)
-    for row in cur.execute('SELECT C_VALUE FROM TPARAMETERS WHERE C_PARAM="TITLE2";'):
-        title = row[0]
-    con.commit()
-    con.close()
 
-    
-    return title, com_list
+    with sqlite3.connect(str(db_path)) as con:
+        cur = con.cursor()
+        cur.execute('SELECT C_NUM, C_FIRST_NAME, C_LAST_NAME, C_CLUB, C_TEAM FROM TCOMPETITORS;')
+        competitors = cur.fetchall()
+        cur.execute('SELECT C_VALUE FROM TPARAMETERS WHERE C_PARAM="TITLE2";')
+        title = f"{cur.fetchone()[0]} - Run {heat}"
+        cur.close()
+    return title, competitors
 
-def get_start_list(eventfile):
+def get_start_list(eventfile, heat):
+    db_path = event_files / eventfile
     startlist = []
     finish_times = {}
 
-    title = "Run 1"
-    con = sqlite3.connect(event_files+eventfile)
-    cur = con.cursor()
-    for row in cur.execute('SELECT C_NUM FROM TSTARTLIST_PARQ2_HEAT1;'):
-        startlist.append(row)
-    for row in cur.execute('SELECT C_NUM, C_TIME, C_STATUS FROM TTIMEINFOS_HEAT1;'):
-        finish_times[row[0]] = [row[1], row[2]]
+    with sqlite3.connect(str(db_path)) as con:
+        cur = con.cursor()
+        query_startlist = f"SELECT C_NUM FROM TSTARTLIST_PARQ2_HEAT{heat};"
+        query_timedata = f"SELECT C_NUM, C_TIME, C_STATUS FROM TTIMEINFOS_HEAT{heat}"
+
+        cur.execute(query_startlist)
+        startlist = [row for row in cur.fetchall()]
+        cur.execute(query_timedata)
+        finish_times = {row[0]: [row[1], row[2]] for row in cur.fetchall()}
+        cur.close()
 
     paired_startlist = [(x[0], y[0]) for x, y in zip(startlist[0::2], startlist[1::2])]
 
     return paired_startlist, finish_times
 
-def get_start_list_dict(startlist,con_per,time_data):
+def get_start_list_dict(startlist, competitors, time_data):
     riders = {}
-    
-    for v,a in enumerate(startlist):
+    for idx, pair in enumerate(startlist):
         riders_tmp = []
-        for g in a:
-            other_data = ""
-            for n in con_per:
-                if g == n[0]:
-                    found = False
-                    for k in time_data:
-                        if k == n[0]:
-                            riders_tmp.append((n[0],n[1],n[2],n[4],n[3],time_data[k][0],True,time_data[k][1]))
-                            found = True
-                    if found == False:
-                        riders_tmp.append((n[0],n[1],n[2],n[4],n[3],0,False,"Not Started"))
+
+        for rider_num in pair:
+            for comp in competitors:
+                if rider_num == comp[0]:
+                    if rider_num in time_data:
+                        riders_tmp.append((*comp, *time_data[rider_num], True))
+                    else:
+                        riders_tmp.append((*comp, 0, False, "Not Started"))
 
         if len(riders_tmp) < 2:
-            riders_tmp.append((0,"Filler", "Filler","Filler","Filler",0,False,"Not Started",))
-        riders[v] = riders_tmp
+            riders_tmp.append((0, "Filler", "Filler", "Filler", "Filler", 0, False, "Not Started"))
+        riders[idx] = riders_tmp
 
     return riders
