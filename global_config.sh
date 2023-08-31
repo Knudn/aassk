ce5f26c09be4f9b4b404b9802d761efa10a8ca3b
Knut Olav <kor@kroland.no>
2023-09-01 01:54:25 +0200

    Update

--- global_config.sh
+++ global_config.sh
@@ -1,7 +1,9 @@
 #The base directory
 export aassk_session_name="Åseral"
 export aassk_project_dir="/home/rock/aassk/new_timing_system"
+
 export aassk_db_location="/home/rock/aassk/db_system"
+#export aassk_db_location="/mnt/test/db_test/"
 
 #Event dir, use /mnt/events/ for production and /mnt/test/ for testing
 export aassk_event_dir="/mnt/test"
