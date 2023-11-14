a50c736962f28c83c403d525c54dbec113cbd709
Knut Olav <kor@kroland.no>
2023-11-14 23:00:44 +0100

    update

--- fast-api/main.py
+++ fast-api/main.py
@@ -159,7 +159,6 @@ async def startup_event():
     Session = sessionmaker(bind=engine)
     session = Session()
     flask_app_url = session.query(Config.orchestrator_endpoint).first()[0]
-    print(flask_app_url)
     flask_app_url = flask_app_url + "/api/init"
 
     try:
