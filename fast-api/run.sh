4268017a5b7ffe22d97bff44fabf30f7d731303e
Knut Olav <kor@kroland.no>
2023-11-15 18:57:27 +0100

    update

--- fast-api/run.sh
+++ fast-api/run.sh
@@ -10,20 +10,14 @@ function check_script_execution() {
         echo "Script has not been executed: Virtual environment 'fastapi_app_env' does not exist."
         return 1
     fi
-
-    source ./fastapi_app_env/bin/activate
-    if ! pip list | grep -q fastapi || ! pip list | grep -q uvicorn || ! pip list | grep -q sqlalchemy; then
-        echo "Script has not been executed: FastAPI, Uvicorn, or SQLAlchemy is not installed in the virtual environment."
-        deactivate
-        return 1
-    fi
-    deactivate
-
+    
     echo "Script has been executed previously."
     return 0
 }
 
 check_script_execution
+
+source ./fastapi_app_env/bin/activate
 
 if [ $? -eq 1 ]; then
     ./install.sh
