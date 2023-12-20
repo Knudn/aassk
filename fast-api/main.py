698a3209e5dce7cb671f73c7ba5bbd3fe7142795
Knut Olav <kor@kroland.no>
2023-12-20 23:48:17 +0100

    update

--- fast-api/main.py
+++ fast-api/main.py
@@ -145,6 +145,7 @@ async def receive_data(request: Request, db: Session = Depends(get_db)):
     for item in data:
         extension = item['url'].lower().split('.')[-1]
         if item['timer'] == "":
+            print("asdasd")
             item['timer'] = "99999999999"
         if "."+extension in image_extensions and "http" not in item['url'].lower():
             new_asset = Asset(name=item['name'], url=flhost+"/api/infoscreen_asset/"+item['url'], timer=item['timer'])
